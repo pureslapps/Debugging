f3f9ac4b8118ba14c11972657a458c4642ba1c27
Jorge Bhagwandeen <jorgebhagwandeen@yahoo.com>
2019-05-08 16:16:48 -0400

    added part two

--- main.js
+++ main.js
@@ -57,3 +57,46 @@ function start(gameOver, computerChoices) {
 
 
 start(gameOver, computerChoices)
+
+
+/////// Part 2
+
+
+function encode(word) {
+  let encode = prompt("Enter your message here.");
+  encode = encode.toLowerCase();
+  //somewhere to store the converted caracters
+  let result = {};
+  //next we loop over each character in the string.
+for(let position of word){
+  //and save the charcode to the array
+  result.push(word.charCodeAt(position-1));
+}
+// and return that list of codes, separated by colons
+return result.list.join(':');
+}
+
+function decode(hash) {
+  let decode = prompt("Enter your code here.");
+  decode = decode.toLowerCase();
+  //split the code by semicolons!
+let arr = hash.splt(':');
+// again somewhere to put our codes
+let result = {};
+//loop over the codes
+for(let code of arr){
+  //change the code back to the string character equivalent
+  result.push(String.fromCharCode(code+1));
+  }
+  return result.list.join('');
+}
+
+
+let message = "I'm trying to send this secret message.";
+let encodedMessage = encode(message);
+let decodedMessage = decode(encodedMessage);
+
+
+console.log(message);
+console.log(encodedMessage);
+console.log(decodedMessage);s
